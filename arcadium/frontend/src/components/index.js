2063ab2d65076ce06e124ef6385a689086835782
Firas Yousfi <yousfify97@gmail.com>
2026-07-17 21:22:34 +0200

    M6 - T4 : include wishlist/backlog status in game details and clean up unused UI components

--- arcadium/frontend/src/components/index.js
+++ arcadium/frontend/src/components/index.js
@@ -17,7 +17,6 @@ export { NavItem } from "./NavItem/NavItem";
 export { SocialLinks } from "./SocialLinks/SocialLinks";
 export { GameImage } from "./GameImage/GameImage";
 export { StoreCard } from "./StoreCard/StoreCard";
-export { NotificationButton } from "./NotificationButton/NotificationButton";
 export { FilterDropdown } from "./FilterDropdown/FilterDropdown";
 export { PriceRangeSlider } from "./PriceRangeSlider/PriceRangeSlider";
 export { Pagination } from "./Pagination/Pagination";
